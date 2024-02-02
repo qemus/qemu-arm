78dae18b4b337470b72e71ca38527027e99a0c93
Kroese <kroese@users.noreply.github.com>
2024-02-02 15:34:39 +0100

    Update display.sh

--- src/display.sh
+++ src/display.sh
@@ -4,21 +4,20 @@ set -Eeuo pipefail
 # Docker environment variables
 
 : "${GPU:="N"}"         # GPU passthrough
-: "${VGA:="std"}"    # VGA adaptor
 : "${DISPLAY:="web"}"   # Display type
 
 case "${DISPLAY,,}" in
   vnc)
-    DISPLAY_OPTS="-display vnc=:0 -vga $VGA"
+    DISPLAY_OPTS="-display vnc=:0"
     ;;
   web)
-    DISPLAY_OPTS="-display vnc=:0,websocket=5700 -vga $VGA"
+    DISPLAY_OPTS="-display vnc=:0,websocket=5700"
     ;;
   none)
-    DISPLAY_OPTS="-display none -vga none"
+    DISPLAY_OPTS="-display none"
     ;;
   *)
-    DISPLAY_OPTS="-display $DISPLAY -vga $VGA"
+    DISPLAY_OPTS="-display $DISPLAY"
     ;;
 esac
 
