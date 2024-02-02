48dd6fad0b8847390f9548fd1f122adb8cd86b22
Kroese <kroese@users.noreply.github.com>
2024-02-02 18:39:35 +0100

    Update proc.sh

--- src/proc.sh
+++ src/proc.sh
@@ -33,7 +33,7 @@ fi
 if [[ "$KVM" != [Nn]* ]]; then
 
   CPU_FEATURES="kvm=on"
-  KVM_OPTS=",accel=kvm,virtualization=true -enable-kvm"
+  KVM_OPTS=",accel=kvm -enable-kvm"
 
   if [[ "${BOOT_MODE,,}" == "windows" ]]; then
 
