a85c2e186e3c60947093eb92cc6546bf1c1cdf11
Kroese <kroese@users.noreply.github.com>
2024-02-02 16:19:47 +0100

    Update proc.sh

--- src/proc.sh
+++ src/proc.sh
@@ -35,7 +35,7 @@ if [[ "$KVM" != [Nn]* ]]; then
   CPU_FEATURES="kvm=on"
   KVM_OPTS=",accel=kvm,virtualization=true -enable-kvm"
 
-  if [[ "${BOOT_MODE,,}" == "windows" ]] || [[ "${BOOT_MODE,,}" == "windows_legacy" ]]; then
+  if [[ "${BOOT_MODE,,}" == "windows" ]]; then
 
     CPU_FEATURES="kvm=on,+hypervisor,+invtsc,l3-cache=on,migratable=no,hv_passthrough"
 
