3b1e34a41e022e935c198d184350981f891a2e80
Kroese <kroese@users.noreply.github.com>
2024-02-02 18:41:47 +0100

    Update entry.sh

--- src/entry.sh
+++ src/entry.sh
@@ -9,9 +9,9 @@ cd /run
 . reset.sh      # Initialize system
 . install.sh    # Get bootdisk
 . disk.sh       # Initialize disks
-. display.sh    # Initialize graphics
 . network.sh    # Initialize network
 . boot.sh       # Configure boot
+. display.sh    # Initialize graphics
 . proc.sh       # Initialize processor
 . config.sh     # Configure arguments
 
