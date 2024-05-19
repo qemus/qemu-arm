96dba61f0ebb707b1ba52f9cd24644f193ccc5b3
Kroese <kroese@users.noreply.github.com>
2024-05-19 21:12:17 +0200

    feat: Display KVM warning on x64 CPU's

--- src/proc.sh
+++ src/proc.sh
@@ -8,7 +8,10 @@ set -Eeuo pipefail
 : "${CPU_MODEL:=""}"
 : "${DEF_MODEL:="neoverse-n1"}"
 
-[[ "${ARCH,,}" != "arm"* ]] && KVM="N"
+ if [[ "${ARCH,,}" != "arm64" ]]; then
+  KVM="N"
+  warn "your CPU architecture is ${ARCH} and cannot provide KVM acceleration for ARM64 instructions, this will cause a major loss of performance."
+fi
 
 if [[ "$KVM" != [Nn]* ]]; then
 
