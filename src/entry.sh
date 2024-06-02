e7b7a7a83f154b31ee7767155429a74926ebfbad
Kroese <kroese@users.noreply.github.com>
2024-06-02 20:58:07 +0200

    feat: Print QEMU version on boot (#100)

--- src/entry.sh
+++ src/entry.sh
@@ -17,7 +17,8 @@ cd /run
 
 trap - ERR
 
-info "Booting image${BOOT_DESC}..."
+version=$(qemu-system-aarch64 --version | head -n 1 | cut -d '(' -f 1 | awk '{ print $NF }')
+info "Booting image${BOOT_DESC} using QEMU v$version..."
 
 if [ -z "$CPU_PIN" ]; then
   exec qemu-system-aarch64 ${ARGS:+ $ARGS}
