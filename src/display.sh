15e1a46162223291d24ff9ec934fd70318a20b9f
Kroese <kroese@users.noreply.github.com>
2025-09-28 17:47:42 +0200

    feat: Make VNC port configurable (#275)

--- src/display.sh
+++ src/display.sh
@@ -6,17 +6,18 @@ set -Eeuo pipefail
 : "${VGA:="ramfb"}"         # VGA adaptor
 : "${DISPLAY:="web"}"       # Display type
 
+port=$(( VNC_PORT - 5900 ))
 [[ "$DISPLAY" == ":0" ]] && DISPLAY="web"
 
 case "${DISPLAY,,}" in
   "vnc" )
-    DISPLAY_OPTS="-display vnc=:0 -device $VGA"
+    DISPLAY_OPTS="-display vnc=:$port -device $VGA"
     ;;
   "web" )
-    DISPLAY_OPTS="-display vnc=:0,websocket=$WSS_PORT -device $VGA"
+    DISPLAY_OPTS="-display vnc=:$port,websocket=$WSS_PORT -device $VGA"
     ;;
   "ramfb" )
-    DISPLAY_OPTS="-display vnc=:0,websocket=$WSS_PORT -device ramfb"
+    DISPLAY_OPTS="-display vnc=:$port,websocket=$WSS_PORT -device ramfb"
     ;;
   "disabled" )
     DISPLAY_OPTS="-display none -device $VGA"
