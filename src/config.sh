a2ffea2da512581a821c64c840ff21e13363e343
Kroese <kroese@users.noreply.github.com>
2026-07-03 16:12:29 +0200

    Refactor USB handling and add UUID support
    Removed USB variable assignment and added UUID handling.

--- src/config.sh
+++ src/config.sh
@@ -7,8 +7,6 @@ set -Eeuo pipefail
 : "${MONITOR:="unix:$QEMU_DIR/monitor.sock,server,wait=off,nodelay"}"
 : "${SMP:="$CPU_CORES,sockets=1,dies=1,cores=$CPU_CORES,threads=1"}"
 
-USB=$(strip "$USB")
-
 msg="Configuring QEMU..."
 html "$msg"
 enabled "$DEBUG" && echo "$msg"
@@ -20,6 +18,7 @@ RAM_OPTS=$(echo "-m ${RAM_SIZE^^}" | sed 's/MB/M/g;s/GB/G/g;s/TB/T/g')
 MON_OPTS="-monitor $MONITOR -name $PROCESS,process=$PROCESS,debug-threads=on -pidfile $QEMU_PID"
 MAC_OPTS="-machine type=${MACHINE},secure=${SECURE},gic-version=max,dump-guest-core=off${KVM_OPTS}"
 
+UUID=$(strip "$UUID")
 [ -n "$UUID" ] && MAC_OPTS+=" -uuid $UUID"
 [ -n "$SM_BIOS" ] && MAC_OPTS+=" $SM_BIOS"
 
