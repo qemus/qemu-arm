b4f4e54d34b4ff177fc8e40309341f84fe750121
Kroese <kroese@users.noreply.github.com>
2026-07-07 01:03:02 +0200

    fix: Allow disabling ARM display device (#368)

--- src/display.sh
+++ src/display.sh
@@ -9,27 +9,30 @@ set -Eeuo pipefail
 VGA=$(strip "$VGA")
 DISPLAY=$(strip "$DISPLAY")
 
+VGA_OPTS=""
+[ -n "$VGA" ] && [[ "${VGA,,}" != "none" ]] && VGA_OPTS="-device $VGA"
+
 port=$(( VNC_PORT - 5900 ))
 [[ "$DISPLAY" == ":0" ]] && DISPLAY="web"
 
 case "${DISPLAY,,}" in
   "vnc" )
-    DISPLAY_OPTS="-display vnc=:$port -device $VGA"
+    DISPLAY_OPTS="-display vnc=:$port ${VGA_OPTS}"
     ;;
   "web" )
-    DISPLAY_OPTS="-display vnc=:$port,websocket=$WSS_PORT -device $VGA"
+    DISPLAY_OPTS="-display vnc=:$port,websocket=$WSS_PORT ${VGA_OPTS}"
     ;;
   "ramfb" )
     DISPLAY_OPTS="-display vnc=:$port,websocket=$WSS_PORT -device ramfb"
     ;;
   "disabled" )
-    DISPLAY_OPTS="-display none -device $VGA"
+    DISPLAY_OPTS="-display none ${VGA_OPTS}"
     ;;
   "none" )
     DISPLAY_OPTS="-display none"
     ;;
   *)
-    DISPLAY_OPTS="-display $DISPLAY -device $VGA"
+    DISPLAY_OPTS="-display $DISPLAY ${VGA_OPTS}"
     ;;
 esac
 
