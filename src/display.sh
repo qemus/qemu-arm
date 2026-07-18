d7dbbec01fc498b375497f097c5fd0748286bbc8
Kroese <kroese@users.noreply.github.com>
2026-07-19 01:33:29 +0200

    Update display.sh

--- src/display.sh
+++ src/display.sh
@@ -5,25 +5,31 @@ set -Eeuo pipefail
 
 : "${VGA:="ramfb"}"         # VGA adaptor
 : "${DISPLAY:="web"}"       # Display type
+: "${LOSSY:="N"}"           # Lossy VNC compression
 
+# Sanitize variables
 VGA=$(strip "$VGA")
+LOSSY=$(strip "$LOSSY")
 DISPLAY=$(strip "$DISPLAY")
 
 VGA_OPTS=""
 [ -n "$VGA" ] && [[ "${VGA,,}" != "none" ]] && VGA_OPTS="-device $VGA"
+
+LOSSY_OPT=""
+enabled "${LOSSY}" && LOSSY_OPT=",lossy=on"
 
 port=$(( VNC_PORT - 5900 ))
 [[ "$DISPLAY" == ":0" ]] && DISPLAY="web"
 
 case "${DISPLAY,,}" in
   "vnc" )
-    DISPLAY_OPTS="-display vnc=:$port ${VGA_OPTS}"
+    DISPLAY_OPTS="-display vnc=:${port}${LOSSY_OPT} ${VGA_OPTS}"
     ;;
   "web" )
-    DISPLAY_OPTS="-display vnc=:$port,websocket=$WSS_PORT ${VGA_OPTS}"
+    DISPLAY_OPTS="-display vnc=:${port},websocket=${WSS_PORT}${LOSSY_OPT} ${VGA_OPTS}"
     ;;
   "ramfb" )
-    DISPLAY_OPTS="-display vnc=:$port,websocket=$WSS_PORT -device ramfb"
+    DISPLAY_OPTS="-display vnc=:${port},websocket=${WSS_PORT}${LOSSY_OPT} -device ramfb"
     ;;
   "disabled" )
     DISPLAY_OPTS="-display none ${VGA_OPTS}"
@@ -32,7 +38,7 @@ case "${DISPLAY,,}" in
     DISPLAY_OPTS="-display none"
     ;;
   *)
-    DISPLAY_OPTS="-display $DISPLAY ${VGA_OPTS}"
+    DISPLAY_OPTS="-display ${DISPLAY} ${VGA_OPTS}"
     ;;
 esac
 
