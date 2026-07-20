44c70741f58494eb7003c9dd5b34e351ba1e643c
Kroese <kroese@users.noreply.github.com>
2026-07-20 09:44:04 +0200

    fix: Prevent races during interactive QEMU shutdown

--- src/entry.sh
+++ src/entry.sh
@@ -44,8 +44,7 @@ fi
 if ! interactive; then
   "${cmd[@]}" ${ARGS:+ $ARGS} &
 else
-  startConsole
-  setsid -w "${cmd[@]}" ${ARGS:+ $ARGS} </dev/null &
+  startQemu "${cmd[@]}" ${ARGS:+ $ARGS}
 fi
 
 pid=$!
